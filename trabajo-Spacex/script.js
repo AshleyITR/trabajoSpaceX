c21066826059525baee9cbfcdab3ab913f1f2fe6
AshleyITR <ashley_brenner@itrsa.com.ar>
2022-08-04 13:50:06 -0300

    8-4 1:50

--- trabajo-Spacex/script.js
+++ trabajo-Spacex/script.js
@@ -8,7 +8,7 @@ let info = 'rockets';
 
 let footerContent = document.getElementById("footerContent");
 
-buttonSearch.addEventListener('click', function () {
+buttonSearch.addEventListener('click', function (info) {
   search = document.getElementById("searchInput").value;
 
   search.toLowerCase();
@@ -17,30 +17,30 @@ buttonSearch.addEventListener('click', function () {
     switch (search) {
       case 'rockets':
         info = search; //se borra luego de tocar el boton, no se guarda
-        
+
         break;
-      case 'capsule':
-       info = search; //se borra luego de tocar el boton, no se guarda
-        break;       
+      case 'dragon':
+        info = search; //se borra luego de tocar el boton, no se guarda
+        break;
     }
-  } else{
+  } else {
     alert('type something');
   }
 });
 console.log(info);
 
-function isActive (isActive, crewTrue){
+function isActive(isActive) {
 
-if(isActive){
-  return 'badge bg-success';
-}else{
-  return 'badge bg-danger';
+  if (isActive) {
+    return 'badge bg-success';
+  } else {
+    return 'badge bg-danger';
+  }
+
 }
+const post = function (info) {
 
-}
-const post = function () {
-
-  fetch(`https://api.spacexdata.com/v4/${info}`) // info = rockets or dragons
+  fetch(`https://api.spacexdata.com/v4/`+ info) // info = rockets or dragons
     .then(response => response.json())  // convertir a json
 
     .then(function (data) {
@@ -78,7 +78,7 @@ const post = function () {
 
 };
 
-post();
+post(info);
 
 
 
@@ -96,62 +96,35 @@ const footer = function () {
       console.log(data);
       footerContent.innerHTML = ` 
       <div class="container text-center text-md-start mt-5">
-      <!-- Grid row -->
       <div class="row mt-3">
-        <!-- Grid column -->
         <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-          <!-- Content -->
           <h6  class="text-uppercase fw-bold mb-4">${data.name}</h6>
-          <p> About: ${data.summary}
-          </p>
+          <p> About: ${data.summary}</p>
         </div>
     
         <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
 
-          <h6 class="text-uppercase fw-bold mb-4">
-            Company data
-          </h6>
-          <p>
-          Founded by: ${data.founder} in ${data.founded}
-          </p>
-          <p>
-          Number of employees: ${data.employees}
-          </p>
-          <p>
-          CEO: ${data.ceo}
-          </p>
-          <p>
-          CTO: ${data.cto}
-          </p>
-          <p>
-          CTO of Propulsion lab: ${data.cto_propulsion}
-          </p>
+          <h6 class="text-uppercase fw-bold mb-4"> Company data</h6>
+          <p>Founded by: ${data.founder} in ${data.founded}</p>
+          <p>Number of employees: ${data.employees}</p>
+          <p>CEO: ${data.ceo}</p>
+          <p>CTO: ${data.cto} </p>
+          <p>CTO of Propulsion lab: ${data.cto_propulsion}</p>
         </div>
   
         <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
         
-          <h6 class="text-uppercase fw-bold mb-4">
-           Equipment data
-          </h6>
-          <p>
-           Launch sites: ${data.launch_sites}
-          </p>
-          <p>
-           Test sites: ${data.test_sites}
-          </p>
-          <p>
-           number of vehicles: ${data.vehicles}
-          </p>
+          <h6 class="text-uppercase fw-bold mb-4"> Equipment data</h6>
+          <p>Launch sites: ${data.launch_sites} </p>
+          <p>Test sites: ${data.test_sites}</p>
+          <p>number of vehicles: ${data.vehicles}</p>
          
         </div>
      
         <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-      
           <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
           <p>${data.headquarters.address}, ${data.headquarters.city}, ${data.headquarters.state}</p>
-          <p>
-         ${data.links.website}
-          </p>
+          <p> ${data.links.website}</p>
           <p> ${data.links.flickr}</p>
           <p>${data.links.twitter}</p>
         </div>
@@ -160,7 +133,7 @@ const footer = function () {
 `;
 
     }
-    )    
+    )
 
     .catch(err => console.log('Solicitud fallida', err));
 
